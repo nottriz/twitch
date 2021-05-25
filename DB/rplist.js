8c3f8c46b93b0ceb84ab7481e3ad5c894bed102a
Triz <kiebusiness1@gmail.com>
2021-05-25 01:35:32 +0100

    Create rplist.js

---
+++ DB/rplist.js
@@ -0,0 +1,6 @@
+export const resourcePacks = {
+    listpacks: 'wyld116, soartexfanver, soartexinvictus',
+    soartexfanver: 'The current resource-pack is Soartex Fanver. Find out more info here - https://soartex.net/',
+    soartexinvictus: 'The current resource-pack is Soartex Invictus. Find out more info here - https://soartex.net/',
+    wyld116: 'The current resource-pack is Wyld116, a custom resourcepack developed by Wyld. This resourcepack is only available to subscribers of Wyld and LadyHana.'
+};
