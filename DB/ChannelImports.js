d281f6a59a44b70ab1e87740e4fafb77f5ab6a0a
Triz <kiebusiness1@gmail.com>
2021-05-25 01:32:04 +0100

    Create ChannelImports.js

---
+++ DB/ChannelImports.js
@@ -0,0 +1,23 @@
+export default {
+    ALMOSTFAE: import('../channels/almostfae/events.js'),
+    BLITZYUK: import('../channels/blitzyuk/events.js'),
+    DFEARTHEREAPER: import('../channels/dfearthereaper/events.js'),
+    DOMOSPLACE: import('../channels/domosplace/events.js'),
+    FINNCAPP: import('../channels/finncapp/events.js'),
+    GWINTHOR: import('../channels/gwinthor/events.js'),
+    IMMP: import('../channels/immp/events.js'),
+    INTIMAE: import('../channels/intimae/events.js'),
+    ITSJUSTTRIZ: import('../channels/itsjusttriz/events.js'),
+    JA_KEELER: import('../channels/ja_keeler/events.js'),
+    JAYROCKBIRD: import('../channels/jayrockbird/events.js'),
+    KIKIISYOURFRIEND: import('../channels/kikiiyourfriend/events.js'),
+    MATRIXIS: import('../channels/matrixis/events.js'),
+    QUEENLIZ09: import('../channels/queenliz09/events.js'),
+    RENINSANE: import('../channels/reninsane/events.js'),
+    RHILOU32: import('../channels/rhilou32/events.js'),
+    SUPERFRAGGLE: import('../channels/superfraggle/events.js'),
+    TECHYGUY: import('../channels/techyguy/events.js'),
+    THEIMPERIALBITGOD: import('../channels/theimperialbitgod/events.js'),
+    TONSTER46346: import('../channels/tonster46346/events.js'),
+    XOBIAS: import('../channels/xobias/events.js')
+};
